50da8b0bb994fe7b58380026573e5f3f12ea3c36
urgirlllbelleee2005 <castriel21@gmail.com>
2025-08-16 17:26:23 -0800

    feat(Phone): add Phone class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'laptop.dart';
+import 'phone.dart';
 
 void main() {
   var laptop = Laptop("Dell", 16);
   laptop.describe();
+
+  var phone = Phone("Samsung", 128);
+  phone.call();
 }
--- lib/phone.dart
+++ lib/phone.dart
@@ -1,1 +1,11 @@
+class Phone {
+  String brand;
+  int storage;
+
+  Phone(this.brand, this.storage);
+
+  void call() {
+    print("Phone: $brand with $storage GB storage is making a call.");
+  }
+}
 
