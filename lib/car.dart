ebe5a2aef0ad900c42b4eff112c94bcc37300ba8
urgirlllbelleee2005 <castriel21@gmail.com>
2025-08-16 17:35:36 -0800

    feat(Car): add Car class

---
+++ lib/car.dart
@@ -0,0 +1,10 @@
+class Car {
+  String model;
+  int year;
+
+  Car(this.model, this.year);
+
+  void drive() {
+    print("Car: $model, made in $year, is driving.");
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'laptop.dart';
 import 'phone.dart';
+import 'car.dart';
 
 void main() {
   var laptop = Laptop("Dell", 16);
@@ -7,4 +8,7 @@ void main() {
 
   var phone = Phone("Samsung", 128);
   phone.call();
+
+var car = Car("Toyota", 2023);
+  car.drive();
 }
