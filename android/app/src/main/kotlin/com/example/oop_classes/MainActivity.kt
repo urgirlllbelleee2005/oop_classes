ef3c7d16b56a72a0499ee8880722c3c46cb1e23b
urgirlllbelleee2005 <castriel21@gmail.com>
2025-08-16 16:57:28 -0800

    feat(Laptop): add Laptop class

--- android/app/src/main/kotlin/com/example/oop_classes/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/oop_classes/MainActivity.kt
@@ -2,4 +2,8 @@ package com.example.oop_classes
 
 import io.flutter.embedding.android.FlutterActivity
 
+<<<<<<< HEAD
 class MainActivity: FlutterActivity()
+=======
+class MainActivity : FlutterActivity()
+>>>>>>> bc04085 (Initial commit)
--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,9 +1,20 @@
 import Cocoa
 import FlutterMacOS
 
+<<<<<<< HEAD
 @NSApplicationMain
+=======
+@main
+>>>>>>> bc04085 (Initial commit)
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+<<<<<<< HEAD
+=======
+
+  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
+>>>>>>> bc04085 (Initial commit)
 }
